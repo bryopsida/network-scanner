18aaeac5a37a0bff4a48524eba9af7bd4bc40672
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-07-23 07:53:28 -0500

    fix jest

--- src/port-scanner/port-scanner.controller.spec.ts
+++ src/port-scanner/port-scanner.controller.spec.ts
@@ -1,14 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { PortScannerController } from './port-scanner.controller'
-import { describe, beforeEach, it, expect } from '@jest/globals'
+import { describe, beforeEach, it, expect, jest } from '@jest/globals'
+import { PortScannerService } from './port-scanner.service'
+jest.mock('./port-scanner.service')
 
 describe('PortScannerController', () => {
   let controller: PortScannerController
+  let mockService: jest.MockedObject<PortScannerService>
 
   beforeEach(async () => {
+    mockService = jest.mocked<PortScannerService>(new PortScannerService(), {
+      shallow: true,
+    })
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PortScannerController],
-    }).compile()
+      providers: [PortScannerService],
+    })
+      .overrideProvider(PortScannerService)
+      .useValue(mockService)
+      .compile()
 
     controller = module.get<PortScannerController>(PortScannerController)
   })
